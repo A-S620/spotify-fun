b6541adb4f41099388ef41dd37620d166ef97f62
A-S620 <aatikaseedat00@gmail.com>
2021-11-02 19:49:14 +0000

    setting up tests

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    roots: ['<rootDir>/src/test'],
+    verbose: true,
+    setupFiles: ['./src/test/setupTests.js'],
+    setupFilesAfterEnv: ['<rootDir>src/test/setupTests.js'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.css$': 'jest-transform-css',
+    },
+    collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    coverageProvider: 'babel',
+    coverageReporters: ['json', 'text', 'lcov', 'clover'],
+    errorOnDeprecated: true,
+    notify: true,
+    resetMocks: true,
+    resetModules: true,
+    testEnvironment: 'node',
+    watchman: true,
+};
