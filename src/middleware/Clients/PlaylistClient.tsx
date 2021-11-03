f20373436826c2c61e4671f5cd727ff5b9ff121e
A-S620 <aatikaseedat00@gmail.com>
2021-11-03 16:06:23 +0000

    Refactored PlaylistClient and unit tests

--- src/middleware/Clients/PlaylistClient.tsx
+++ src/middleware/Clients/PlaylistClient.tsx
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
-export const PlaylistClient = async (playlistId: string, accessToken: string) => {
-    try {
-        const response = await axios.get('https://api.spotify.com/v1/playlists/' + playlistId, {
-            headers: {
-                Accept: 'application/json',
-                Authorization: 'Bearer ' + accessToken,
-                'Content-Type': 'application/json',
-            },
-        });
-        return response.data;
-    } catch (error) {
-        // @ts-ignore
-        const { response } = error;
-        if (response) {
-            const errorResponseData = response.data.error as { status: number; message: string };
-            return new Error(`${errorResponseData.status}: ${errorResponseData.message}`);
+export default class PlaylistClient {
+    static getPlaylistById = async (playlistId: string, accessToken: string) => {
+        try {
+            const response = await axios.get('https://api.spotify.com/v1/playlists/' + playlistId, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: 'Bearer ' + accessToken,
+                    'Content-Type': 'application/json',
+                },
+            });
+            return response.data;
+        } catch (error) {
+            // @ts-ignore
+            const { response } = error;
+            if (response) {
+                const errorResponseData = response.data.error as { status: number; message: string };
+                return new Error(`${errorResponseData.status}: ${errorResponseData.message}`);
+            }
+            return new Error('Request Failed');
         }
-        return new Error('Request Failed');
-    }
-};
+    };
+}
--- src/tests/middleware/Clients/PlaylistClient.test.tsx
+++ src/tests/middleware/Clients/PlaylistClient.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { AuthClient } from '../../../middleware/Clients/AuthClient';
-import { PlaylistClient } from '../../../middleware/Clients/PlaylistClient';
+import PlaylistClient from '../../../middleware/Clients/PlaylistClient';
 import keyDetails from '../../../util/keyDetails';
 
 let accessToken: string;
@@ -8,12 +8,14 @@ beforeAll(async () => {
     accessToken = await AuthClient(keyDetails.clientID, keyDetails.clientSecret);
 });
 describe('PlaylistClient client component', () => {
-    it('should return the playlist when a valid playlistId is used', async () => {
-        const response = await PlaylistClient('3kwtMOO3XycjEyqsvzqbDp', accessToken);
-        expect(response.name).toBe('Just Blue Side');
-    });
-    it('should throw an error when an invalid playlistId is used', async () => {
-        const response = await PlaylistClient('test', accessToken);
-        expect(response.toString()).toEqual('Error: 404: Invalid playlist Id');
+    describe('getPlaylistById', () => {
+        it('should return the playlist when a valid playlistId is used', async () => {
+            const response = await PlaylistClient.getPlaylistById('3kwtMOO3XycjEyqsvzqbDp', accessToken);
+            expect(response.name).toBe('Just Blue Side');
+        });
+        it('should throw an error when an invalid playlistId is used', async () => {
+            const response = await PlaylistClient.getPlaylistById('test', accessToken);
+            expect(response.toString()).toEqual('Error: 404: Invalid playlist Id');
+        });
     });
 });
