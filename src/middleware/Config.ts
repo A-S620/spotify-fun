988b99990ae37e0ff15ed5ea52ba5f75b51589a9
A-S620 <aatikaseedat00@gmail.com>
2021-11-03 17:04:22 +0000

    added SPOTIFY_CLIENT_ID to npm run test in package.json refactored unit test to use process.env.SPOTIFY_CLIENT_ID

--- src/middleware/Config.ts
+++
@@ -1,6 +0,0 @@
-const _env = window as any;
-
-export class Config {
-    static SPOTIFY_CLIENT_ID = _env.REACT_APP_SPOTIFY_CLIENT_ID;
-    static SPOTIFY_CLIENT_SECRET = _env.REACT_APP_SPOTIFY_CLIENT_SECRET;
-}
