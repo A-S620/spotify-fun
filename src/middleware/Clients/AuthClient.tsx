cda87c0a045df6806e9bd11d58be006b78cb29d3
A-S620 <aatikaseedat00@gmail.com>
2021-11-03 16:09:17 +0000

    Refactored AuthClient and unit tests

--- src/middleware/Clients/AuthClient.tsx
+++ src/middleware/Clients/AuthClient.tsx
@@ -1,32 +1,33 @@
 import axios from 'axios';
 import qs from 'qs';
 
-export const AuthClient = async (clientId: string, clientSecret: string) => {
-    const headers = {
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-            username: clientId,
-            password: clientSecret,
-        },
-    };
-    const data = {
-        grant_type: 'client_credentials',
-    };
+export default class AuthClient {
+    static getAccessToken = async (clientId: string, clientSecret: string) => {
+        const headers = {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: clientId,
+                password: clientSecret,
+            },
+        };
+        const data = {
+            grant_type: 'client_credentials',
+        };
 
-    try {
-        const response = await axios.post('https://accounts.spotify.com/api/token', qs.stringify(data), headers);
-        return response.data.access_token;
-    } catch (error) {
-        // @ts-ignore
-        const { response } = error;
-        if (response) {
-            const errorResponseData = response.data as { error: string; error_description: string };
-            console.log(Object.entries(response.data));
-            return new Error(`${errorResponseData.error}: ${errorResponseData.error_description}`);
+        try {
+            const response = await axios.post('https://accounts.spotify.com/api/token', qs.stringify(data), headers);
+            return response.data.access_token;
+        } catch (error) {
+            // @ts-ignore
+            const { response } = error;
+            if (response) {
+                const errorResponseData = response.data as { error: string; error_description: string };
+                return new Error(`${errorResponseData.error}: ${errorResponseData.error_description}`);
+            }
+            return new Error('Request Failed');
         }
-        return new Error('Request Failed');
-    }
-};
+    };
+}
--- src/tests/middleware/Clients/AuthClient.test.tsx
+++ src/tests/middleware/Clients/AuthClient.test.tsx
@@ -1,14 +1,16 @@
 import '@testing-library/jest-dom';
-import { AuthClient } from '../../../middleware/Clients/AuthClient';
+import AuthClient from '../../../middleware/Clients/AuthClient';
 import keyDetails from '../../../util/keyDetails';
 
 describe('AuthClient component', () => {
-    test('should throw an error when the clientId and clientSecret are invalid', async () => {
-        const response = await AuthClient('fakeId', 'fakeClient');
-        expect(response.toString()).toBe('Error: invalid_client: Invalid client');
-    });
-    test('should respond with a string token when the clientId and clientSecret are valid', async () => {
-        const response = await AuthClient(keyDetails.clientID, keyDetails.clientSecret);
-        expect(typeof response).toBe('string');
+    describe('getAccessToken', () => {
+        test('should throw an error when the clientId and clientSecret are invalid', async () => {
+            const response = await AuthClient.getAccessToken('fakeId', 'fakeClient');
+            expect(response.toString()).toBe('Error: invalid_client: Invalid client');
+        });
+        test('should respond with a string token when the clientId and clientSecret are valid', async () => {
+            const response = await AuthClient.getAccessToken(keyDetails.clientID, keyDetails.clientSecret);
+            expect(typeof response).toBe('string');
+        });
     });
 });
