5629c120a82ef50aa2b691510f6531ada459ad17
A-S620 <aatikaseedat00@gmail.com>
2021-11-03 16:50:20 +0000

    Refactored Config.ts and env-config.js

--- src/middleware/Config.ts
+++ src/middleware/Config.ts
@@ -1,6 +1,6 @@
 const _env = window as any;
 
 export class Config {
-    static SPOTIFY_CLIENT_ID = _env.SPOTIFY_CLIENT_ID;
-    static SPOTIFY_CLIENT_SECRET = _env.SPOTIFY_CLIENT_SECRET;
+    static SPOTIFY_CLIENT_ID = _env.REACT_APP_SPOTIFY_CLIENT_ID;
+    static SPOTIFY_CLIENT_SECRET = _env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 }
