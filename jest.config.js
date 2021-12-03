f334a27e69cf613ffcabfbef119841e0bf3fbba3
A-S620 <aatikaseedat00@gmail.com>
2021-12-03 06:45:34 +0000

    pipeline fix

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     roots: ['<rootDir>/src/tests'],
     verbose: true,
     setupFiles: ['./src/tests/setupTests.js'],
-    setupFilesAfterEnv: ['<rootDir>src/test/setupTests.js'],
+    setupFilesAfterEnv: ['<rootDir>src/tests/setupTests.js'],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     transform: {
