8d636a5f4ee11c114849a0697f9065d52f4e9fb5
A-S620 <aatikaseedat00@gmail.com>
2022-01-17 20:24:22 +0000

    Created AnalysisDialog

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 import { ITrack } from '../../Interfaces/ITrack';
+import AnalysisDialog from '../AnalysisDailog/AnalysisDialog';
 
 interface IPlaylist {
     playlistName: string;
@@ -18,10 +19,11 @@ export default function Playlist(props: IPlaylist) {
         <div className="Playlist">
             <h2>Songs to Analyse</h2>
             <TrackList trackList={props.playlistTracks} onRemove={(track) => props.onRemove(track)} isRemoval={true} />
-            <button className="Playlist-save" onClick={() => props.onSave(props.playlistTracks)}>
-                {' '}
-                ANALYSE SONGS
-            </button>
+            {/*<button className="Playlist-save" onClick={() => props.onSave(props.playlistTracks)}>*/}
+            {/*    {' '}*/}
+            {/*    ANALYSE SONGS*/}
+            {/*</button>*/}
+            <AnalysisDialog />
         </div>
     );
 }
